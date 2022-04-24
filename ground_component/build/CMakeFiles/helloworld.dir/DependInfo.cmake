ce732f934e55afe29ec0b0589336fb573eb461ed
Rijesh Augustine <rijesh@ualberta.ca>
2022-04-24 13:41:11 -0600

    updated cmake

---
+++ ground_component/build/CMakeFiles/helloworld.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rijesh/research/compVisionModule/ground_component/build/helloworld_autogen/mocs_compilation.cpp" "/home/rijesh/research/compVisionModule/ground_component/build/CMakeFiles/helloworld.dir/helloworld_autogen/mocs_compilation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "helloworld_autogen/include"
+  "/usr/local/Qt-6.2.3/include/QtWidgets"
+  "/usr/local/Qt-6.2.3/include"
+  "/usr/local/Qt-6.2.3/include/QtCore"
+  "/usr/local/Qt-6.2.3/mkspecs/linux-g++"
+  "/usr/local/Qt-6.2.3/include/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
